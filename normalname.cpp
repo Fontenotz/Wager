17e1b3aeb339ff3c849b241914bea1132123638e
Mike Dank <Famicoman@gmail.com>
2015-04-03 13:23:33 -0400

    Initial commit.
    Initial commit.

---
+++ normalname.cpp
@@ -0,0 +1,43 @@
+char finald;
+char fp;
+int wager;
+
+void wager()
+{
+cout<<"you have this much money:$"<<money<<endl;
+cout<<endl;
+cout<<"would you like to make a wager?(Y or n)";
+cin>>fp;
+if(fo=='y')
+{
+cout<<"please input your wager ($10,20,50)"<<endl;
+cout<<"$";
+cin>>wager;
+if (wager==10//wager==20//wager==50)
+{
+cout<<"you have bet $<<wager<<endl;
+cout<<"type Y to continue or n to start over"<<endl;
+cin>>finald;
+if (finald=='y')
+{
+system("cls")
+void main();
+}
+else
+void wager();
+}
+else
+cout<<"please enter an amount of 10,20, or 50"<<endl;
+cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
+void wager();
+}
+else
+cout<<"mode is set to free play,type y to continue"<<endl;
+cin>>finald=='y')
+{
+system("cls")
+void main();
+}
+else
+void wager();
+}
